8f782c34ef784f38ac978ebfb39482ddbbd824cc
armandios19 <fooseaov@gmail.com>
2022-03-18 10:22:50 -0600

    funciones para df

---
+++ funciones/one_nutri_df.R
@@ -0,0 +1,56 @@
+zone_nutri_df <- function(x = ensanut_limpia, food, zone){
+  pre <- x %>%
+    select(food, edad_categorica, region) %>%
+    filter(region == zone) %>%
+    group_by(edad_categorica, region) %>%
+    count() %>%
+    filter(edad_categorica == "preescolares") %>%
+    summarise(total = sum(freq))
+
+  pre_total <- pre$total
+
+
+  esc <-x %>%
+    select(food, edad_categorica, region) %>%
+    filter(region == zone) %>%
+    group_by(edad_categorica, region) %>%
+    count() %>%
+    filter(edad_categorica == "escolares") %>%
+    summarise(total = sum(freq))
+
+  esc_total <- esc$total
+
+  ado <- x %>%
+    select(food, edad_categorica, region) %>%
+    filter(region == zone) %>%
+    group_by(edad_categorica, region) %>%
+    count() %>%
+    filter(edad_categorica == "adolescentes") %>%
+    summarise(total = sum(freq))
+
+  ado_total <- ado$total
+
+  adul <- x %>%
+    select(food, edad_categorica, region) %>%
+    filter(region == zone) %>%
+    group_by(edad_categorica, region) %>%
+    count() %>%
+    filter(edad_categorica == "adultos") %>%
+    summarise(total = sum(freq))
+
+  adul_total <- adul$total
+
+  x %>%
+    select(food, edad_categorica, region) %>%
+    filter(x[,(colnames(x)%in%c(food))] == 1,
+           region == zone) %>%
+    group_by(edad_categorica, region) %>%
+    count() %>%
+    mutate(porcentaje = case_when(edad_categorica == "adolescentes" ~ freq/ado_total,
+                                  edad_categorica == "adultos" ~ freq/adul_total,
+                                  edad_categorica == "escolares" ~ freq/esc_total,
+                                  edad_categorica == "preescolares" ~ freq/pre_total),
+           edad_categorica = factor(edad_categorica, levels = c("preescolares",
+                                                                "escolares", "adolescentes",
+                                                                "adultos")))
+}
