681f91e1fb5a62da4f629ceab7bb39b9836e82f6
armandios19 <fooseaov@gmail.com>
2022-05-03 09:14:08 -0500

    más pruebas

--- scripts/test.R
+++ scripts/test.R
@@ -1,2 +1,11 @@
 nutri_plot(food = "agua")
 
+nutri_df(food = "agua")
+
+
+state_nutri_plot(food = "frutas", state = "Aguascalientes")
+state_nutri_df(food = "frutas", state = "Aguascalientes")
+
+
+filter_nutri_plot(food = "frutas", state = "Chihuahua", sex = "hombre", domain = "urbano")
+filter_nutri_df(food = "frutas", state = "Chihuahua", sex = "hombre", domain = "urbano")
