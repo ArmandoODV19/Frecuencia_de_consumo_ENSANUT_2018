920bf1542141da480e3d7955df352be5c8f93284
armandios19 <fooseaov@gmail.com>
2022-05-03 09:15:35 -0500

    test para funcion de area

--- scripts/test.R
+++ scripts/test.R
@@ -9,3 +9,6 @@ state_nutri_df(food = "frutas", state = "Aguascalientes")
 
 filter_nutri_plot(food = "frutas", state = "Chihuahua", sex = "hombre", domain = "urbano")
 filter_nutri_df(food = "frutas", state = "Chihuahua", sex = "hombre", domain = "urbano")
+
+state_area_nutri_plot(food = "frutas", state = "Morelos", domain = "rural")
+state_area_nutri_df(food = "frutas", state = "Morelos", domain = "rural")
