d6951991bf1d5e73144f03926a00cb005965ebac
armandios19 <fooseaov@gmail.com>
2022-06-20 13:50:01 -0500

    cambio de freq por n

--- funciones/nutri_plot_all_states.R
+++ funciones/nutri_plot_all_states.R
@@ -4,10 +4,10 @@ nutri_plot_all_states <- function(x = ensanut_limpia, food, x_name = "edad", y_n
     filter(agua == 1) %>%
     group_by(edad_categorica, entidad) %>%
     count() %>%
-    mutate(porcentaje = case_when(edad_categorica == "adolescentes" ~ freq/5182,
-                                  edad_categorica == "adultos" ~ freq/15791,
-                                  edad_categorica == "escolares" ~ freq/5989,
-                                  edad_categorica == "preescolares" ~ freq/3068),
+    mutate(porcentaje = case_when(edad_categorica == "adolescentes" ~ n/5182,
+                                  edad_categorica == "adultos" ~ n/15791,
+                                  edad_categorica == "escolares" ~ n/5989,
+                                  edad_categorica == "preescolares" ~ n/3068),
            edad_categorica = factor(edad_categorica, levels = c("preescolares",
                                                                 "escolares", "adolescentes",
                                                                 "adultos"))) %>%
