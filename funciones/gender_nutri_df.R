38cf50d15f176bd47a8f808b43b6709c77c482f5
armandios19 <fooseaov@gmail.com>
2022-06-20 13:43:33 -0500

    agregando n en lugar de freq

--- funciones/gender_nutri_df.R
+++ funciones/gender_nutri_df.R
@@ -7,7 +7,7 @@ gender_nutri_df <- function(x = ensanut_limpia, food,state, domain){
     count() %>%
     filter(edad_categorica == "preescolares",
            sexo == "hombre") %>%
-    summarise(total = sum(freq))
+    summarise(total = sum(n))
 
   pre_hombre_total <- pre_hombre$total
 
@@ -19,7 +19,7 @@ gender_nutri_df <- function(x = ensanut_limpia, food,state, domain){
     count() %>%
     filter(edad_categorica == "preescolares",
            sexo == "mujer") %>%
-    summarise(total = sum(freq))
+    summarise(total = sum(n))
 
   pre_mujer_total <- pre_mujer$total
 
@@ -32,7 +32,7 @@ gender_nutri_df <- function(x = ensanut_limpia, food,state, domain){
     count() %>%
     filter(edad_categorica == "escolares",
            sexo == "hombre") %>%
-    summarise(total = sum(freq))
+    summarise(total = sum(n))
 
   esc_hombre_total <- esc_hombre$total
 
@@ -44,7 +44,7 @@ gender_nutri_df <- function(x = ensanut_limpia, food,state, domain){
     count() %>%
     filter(edad_categorica == "escolares",
            sexo == "mujer") %>%
-    summarise(total = sum(freq))
+    summarise(total = sum(n))
 
   esc_mujer_total <- esc_mujer$total
 
@@ -56,7 +56,7 @@ gender_nutri_df <- function(x = ensanut_limpia, food,state, domain){
     count() %>%
     filter(edad_categorica == "adolescentes",
            sexo == "hombre") %>%
-    summarise(total = sum(freq))
+    summarise(total = sum(n))
 
   ado_hombre_total <- ado_hombre$total
 
@@ -68,7 +68,7 @@ gender_nutri_df <- function(x = ensanut_limpia, food,state, domain){
     count() %>%
     filter(edad_categorica == "adolescentes",
            sexo == "mujer") %>%
-    summarise(total = sum(freq))
+    summarise(total = sum(n))
 
   ado_mujer_total <- ado_mujer$total
 
@@ -80,7 +80,7 @@ gender_nutri_df <- function(x = ensanut_limpia, food,state, domain){
     count() %>%
     filter(edad_categorica == "adultos",
            sexo == "hombre") %>%
-    summarise(total = sum(freq))
+    summarise(total = sum(n))
 
   adul_hombre_total <- adul_hombre$total
 
@@ -92,7 +92,7 @@ gender_nutri_df <- function(x = ensanut_limpia, food,state, domain){
     count() %>%
     filter(edad_categorica == "adultos",
            sexo == "mujer") %>%
-    summarise(total = sum(freq))
+    summarise(total = sum(n))
 
   adul_mujer_total <- adul_mujer$total
 
@@ -103,14 +103,14 @@ gender_nutri_df <- function(x = ensanut_limpia, food,state, domain){
            area == domain) %>%
     group_by(edad_categorica, entidad, sexo, area) %>%
     count() %>%
-    mutate(porcentaje = case_when(edad_categorica == "adolescentes" & sexo == "hombre" ~ freq/ado_hombre_total,
-                                  edad_categorica == "adolescentes" & sexo == "mujer" ~ freq/ado_mujer_total,
-                                  edad_categorica == "adultos" & sexo == "hombre" ~ freq/adul_hombre_total,
-                                  edad_categorica == "adultos" & sexo == "mujer" ~ freq/adul_mujer_total,
-                                  edad_categorica == "escolares" & sexo == "hombre" ~ freq/esc_hombre_total,
-                                  edad_categorica == "escolares" & sexo == "mujer" ~ freq/esc_mujer_total,
-                                  edad_categorica == "preescolares" & sexo == "hombre" ~ freq/pre_hombre_total,
-                                  edad_categorica == "preescolares" & sexo == "mujer" ~ freq/pre_mujer_total),
+    mutate(porcentaje = case_when(edad_categorica == "adolescentes" & sexo == "hombre" ~ n/ado_hombre_total,
+                                  edad_categorica == "adolescentes" & sexo == "mujer" ~ n/ado_mujer_total,
+                                  edad_categorica == "adultos" & sexo == "hombre" ~ n/adul_hombre_total,
+                                  edad_categorica == "adultos" & sexo == "mujer" ~ n/adul_mujer_total,
+                                  edad_categorica == "escolares" & sexo == "hombre" ~ n/esc_hombre_total,
+                                  edad_categorica == "escolares" & sexo == "mujer" ~ n/esc_mujer_total,
+                                  edad_categorica == "preescolares" & sexo == "hombre" ~ n/pre_hombre_total,
+                                  edad_categorica == "preescolares" & sexo == "mujer" ~ n/pre_mujer_total),
            edad_categorica = factor(edad_categorica, levels = c("preescolares",
                                                                 "escolares", "adolescentes",
                                                                 "adultos")))
